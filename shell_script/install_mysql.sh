3936bb9392f162a0f074465c7de3ca91418f75e0
youzhiqiang <youzhiqiang@localhost.localdomain>
2020-07-05 15:11:12 +0800

    20200705 by thinkpad

--- shell_script/install_mysql.sh
+++ shell_script/install_mysql.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+echo
+echo "The script begin run...."
+sleep 2
+CURRENT_USER=$(whoami)
+if [ "$CURRENT_USER" != "root" ];then
+	echo -e "\033[31m the current user is $CURRENT_USER,please use root to run this script! \033[0m"
+	echo
+	echo "leaving script..."
+	exit 1
+fi
 export PATH=/root/perl5/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin
 PACKAGE_DIR=/home/installpackage/mysql
 file1=mysql-community-common-5.7.13-1.el6.x86_64.rpm
@@ -11,34 +21,66 @@ do
 		echo "${i} is not exists,ready to exit....." && exit 1
 	fi
 done
-echo $(rpm -qa | grep mariadb) | xargs -n 1  rpm -e --nodeps
-echo $(rpm -qa | grep mysql) | xargs -n 1 rpm -e --nodeps
+systemctl status mysqld.service >/dev/null
+if [ $? -eq 0 ]
+then	
+	echo 
+	echo -e "\033[31m \t\t This is a mysql is running.. \033[0m"
+	echo -e "\033[32m \t\t Do you want stop it and remove it?[Y|N]:  \033[0m"
+	read -t 10 ANSWER
+	case $ANSWER in
+	Y|y|Yes|YEs|YES|yes|yEs|yeS|yES)
+		
+		;;
+	*)
+		echo
+		echo "you chose 'no',we leaving the script..."
+		echo "exit."
+		exit
+		;;
+	esac
+	systemctl stop mysqld.service
+fi 
+echo $(rpm -qa | grep mariadb 2>/dev/null) | xargs -n 1  rpm -e --nodeps
+echo $(rpm -qa | grep mysql 2>/dev/null) | xargs -n 1  rpm -e --nodeps
+[ "#?" -eq 0 ] && echo -e "\033[32m \t\t\t Remove the existed mysql successfully! \033[0m"
 cd ${PACKAGE_DIR}
-rpm -ivh ${file1} --nodeps --force && rpm -ivh ${file2}  && rpm -ivh ${file3} && rpm -ivh ${file4} || echo "install failed !!"
+echo
+echo -e "\033[32m Now install mysql.... \033[0m"
+sleep 1
+ rpm -ivh ${file1} --nodeps --force &&   rpm -ivh ${file2}  &&  rpm -ivh ${file3} &&  rpm -ivh ${file4} || echo "install failed !!"
+TEMP_PASSWORD=$(awk '/temporary password/{print $NF}' /var/log/mysqld.log)
+LOGIN_PASSWORD="root@123"
+#sed -i  '/^\[mysqld\]$/a skip-grant-tables' /etc/my.cnf  ##不指定行号，使用正则匹配
 /etc/init.d/mysqld start
-echo "mysqld first start successfully!"
-/etc/init.d/mysqld stop
-echo "mysqld first stop successfully!"
-#mysqld_safe --user=mysql --skip-grant-tables &
-#echo -e "\n"
-sed -i  '/^\[mysqld\]$/a skip-grant-tables' /etc/my.cnf  ##不指定行号，使用正则匹配
-/etc/init.d/mysqld start
-/usr/bin/expect <<-EOF
+
+if ! which expect
+then
+	echo "Current system is not expect service,now install expect,wait a minute..."
+	sleep 2
+	yum install expect -y	
+fi
+
+/usr/bin/expect<<EOF
 spawn mysql -u root -p
 expect "*:*"
-send "\n;\r"
+send "${TEMP_PASSWORD};\r"
 expect "*>*"
-send "set password=password('root@1234');\r"
+send "set global validate_password_policy=0;\r"
 expect "*>*"
-send "grant all on *.* to root@'%' identified by 'root@1234';\r"
+send "set global validate_password_length=1;\r"
+expect "*>*"
+send "set password=password('${LOGIN_PASSWORD}');\r"
+expect "*>*"
+send "grant all on *.* to 'root'@'%' identified by '${LOGIN_PASSWORD}' with grant option ;\r"
 expect "*>*"
 send "flush privileges;\r"
 expect "*>*"
 send "quit;\r"
 EOF
-sed -i '/^skip-grant-tables$/d' /etc/my.cnf
+#sed -i '/^skip-grant-tables$/d' /etc/my.cnf
 systemctl restart mysqld.service ##不指定行号，使用正则匹配
-mysql -uroot -proot@1234 -e "show databases;"
+mysql -uroot -p${LOGIN_PASSWORD} -e "show databases;"
 [ "$?" == "0"  ] && echo  -e "\033[32m mysql installed successfully!\033[0m" || echo -e "\033[31m mysql install failed!\033[0m"
 
 
---
+++ shell_script/test_sudo.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export PATH=/root/perl5/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin
+echo $(whoami)
